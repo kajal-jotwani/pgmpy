312e0334ab1574de7c6c9f3996a8385ecebbbcb0
Ankur Ankan <ankurankan@gmail.com>
2026-03-19 22:56:15 +0100

    Updates tests

--- pgmpy/tests/test_ci_tests/test_pearsonr.py
+++ pgmpy/tests/test_ci_tests/test_pearsonr.py
@@ -16,19 +16,19 @@ class TestPearsonr(unittest.TestCase):
         self.df_ind = pd.DataFrame(np.random.randn(1000, 3), columns=["X", "Y", "Z"])
 
         Z = rng.normal(10000)
-        X = 3 * Z + rng.normal(loc=0, scale=0.1, size=10000)
-        Y = 2 * Z + rng.normal(loc=0, scale=0.1, size=10000)
+        X = 0.3 * Z + rng.normal(loc=0, scale=0.1, size=10000)
+        Y = 0.2 * Z + rng.normal(loc=0, scale=0.1, size=10000)
         self.df_cind = pd.DataFrame({"X": X, "Y": Y, "Z": Z})
 
         Z1 = rng.normal(10000)
         Z2 = rng.normal(10000)
-        X = 3 * Z1 + 2 * Z2 + rng.normal(loc=0, scale=0.1, size=10000)
-        Y = 2 * Z1 + 3 * Z2 + rng.normal(loc=0, scale=0.1, size=10000)
+        X = 0.3 * Z1 + 0.2 * Z2 + rng.normal(loc=0, scale=0.1, size=10000)
+        Y = 0.2 * Z1 + 0.3 * Z2 + rng.normal(loc=0, scale=0.1, size=10000)
         self.df_cind_mul = pd.DataFrame({"X": X, "Y": Y, "Z1": Z1, "Z2": Z2})
 
         X = rng.normal(10000)
         Y = rng.normal(10000)
-        Z = 2 * X + 2 * Y + rng.normal(loc=0, scale=0.1, size=10000)
+        Z = 0.2 * X + 0.2 * Y + rng.normal(loc=0, scale=0.1, size=10000)
         self.df_vstruct = pd.DataFrame({"X": X, "Y": Y, "Z": Z})
 
     def test_pearsonr(self):
