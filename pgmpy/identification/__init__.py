06c1f2ce8cd996852f73b50add180bcc3a961f1b
kajal-jotwani <kajaljotwani06@gmail.com>
2026-06-10 18:08:36 +0530

    minor fix

--- pgmpy/identification/__init__.py
+++ pgmpy/identification/__init__.py
@@ -1,6 +1,6 @@
 from ._base import BaseIdentification  # isort: skip  # noqa: E402
 from .adjustment import Adjustment
 from .frontdoor import Frontdoor
-from .probability_expression import ProbabilityExpression
+from .probability_expression import ProbabilityExpressionTree
 
-__all__ = ["BaseIdentification", "Adjustment", "Frontdoor", "ProbabilityExpression"]
+__all__ = ["BaseIdentification", "Adjustment", "Frontdoor", "ProbabilityExpressionTree"]
